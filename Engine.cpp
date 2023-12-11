ece875405f3dc3198334dcedadcbd35a25f80d70
Hurta <80070683+hurta03@users.noreply.github.com>
2023-12-10 21:19:17 -0800

    Update Engine.cpp
    Fixed particles not spawning

--- Engine.cpp
+++ Engine.cpp
@@ -2,6 +2,32 @@
 
 void Engine::input()
 {
+    Event event;
+    const int particleCount = 5;
+    if (Keyboard::isKeyPressed(Keyboard::Escape))
+    {
+        m_Window.close();
+    }
+    while (m_Window.pollEvent(event))
+    {
+        if (event.type == sf::Event::MouseButtonPressed)
+        {
+            if (event.mouseButton.button == sf::Mouse::Left)
+            {
+                for (int i = 0; i < particleCount; i++)
+                {
+                    int numPoints = rand() % 50 + 25;
+                    Vector2i mousePos = Vector2i(event.mouseButton.x, event.mouseButton.y);
+                    cout << "Creating Particle At: " << endl;
+                    cout << mousePos.x << " " << mousePos.y << endl;
+                    Particle P(m_Window, particleCount, mousePos);
+                    m_particles.push_back(P);
+                }
+
+            }
+        }
+
+    }
 }
 
 void Engine::update(float dtAsSeconds)
@@ -9,11 +35,14 @@ void Engine::update(float dtAsSeconds)
     vector<Particle>::iterator it;
     //Fix / Do not increment iterator
     int i = 0;
-    for (auto it = m_particles.begin(); it != m_particles.end(); ++it, i++)
+    //for (auto it = m_particles.begin(); it != m_particles.end(); ++it, i++)
+    for (vector<Particle>::iterator it = m_particles.begin(); it != m_particles.end();)
     {
-        if (m_particles.at(i).getTTL() > 0)
+        if (it->getTTL() > 0)
         {
-            m_particles.at(i).update(dtAsSeconds);
+            it->update(dtAsSeconds);
+            it++;
+            
         }
         else
         {
@@ -48,36 +77,17 @@ void Engine::run()
     Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
     p.unitTests();
     cout << "Unit tests complete.  Starting engine..." << endl;
-    Event event;
-    const int particleCount = 5;
+
     while (m_Window.isOpen())
     {
         EngineClock.restart();
         Time time = EngineClock.getElapsedTime();
         float seconds = time.asSeconds();
-        if (Keyboard::isKeyPressed(Keyboard::Escape))
-        {
-            m_Window.close();
-        }
-
-        while (m_Window.pollEvent(event))
-        {
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
-                    for (int i = 0; i < particleCount; i++)
-                    {
-                        int numPoints = rand() % 50 + 25;
-                        Vector2i mousePos = Vector2i(event.mouseButton.x, event.mouseButton.y);
-                        cout << "Creating Particle At: " << endl;
-                        cout << mousePos.x << " " << mousePos.y << endl;
-                    }
-
-                }
-            }
-
-        }
+        cout << m_particles.size() << " ";
+        input();
+        update(time.asSeconds()); // dtseconds
+        draw();
+        
 
     }
 }
